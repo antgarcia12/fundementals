4703d5ee69c6b78579eced3d974d96dfe0a1c528
Anthony Garcia <garcia.anthony577@gmail.com>
2018-10-01 14:33:12 -0600

    practicing variables data and types using js

---
+++ v-and-d.js
@@ -0,0 +1,5 @@
+let varl = "2";
+let var2 = 5;
+let result = var1 + var2;
+
+console.log(result);
---
+++ variable-and-data.js
@@ -0,0 +1,10 @@
+let obj = {
+	key1: "value",
+	key2: 4,
+	boolean: true,
+	obj2: {
+		obj2key1: "Internal Object Value",
+	}
+};
+
+console.log(obj.obj2.objkey1);
